719098c75b75c24161070bb8136a1a64d5de36dc
sebbycake <leehur_sebastian@hotmail.com>
2022-12-13 19:14:18 +0800

    Add layout to all pages

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+import Image from 'next/image'
+
+function Header() {
+  return (
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+        <h1>NavBar</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+function Layout({ children }) {
+	return (
+		<div>
+			<Header />
+			{ children }
+		</div>
+	);
+}
+
+export default Layout;
+
