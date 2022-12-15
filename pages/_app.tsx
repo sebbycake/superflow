41132c202b8b25184918539f93beb7489af928af
simba yip <simbayippy@gmail.com>
2022-12-15 19:26:42 +0800

    adding wallet connect, wagmi/rainbowkit dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,45 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import Layout from "../components/Layout"
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, goerli } from 'wagmi/chains';
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
+const { chains, provider } = configureChains(
+	[mainnet, polygon, optimism, arbitrum, goerli],
+	//api key to add to .env later. but adding here so everyone can use for now
+	[alchemyProvider({ apiKey: "9RscFf-M5Wz5EMfVOcX9Wnic6kAUMB6r" }), publicProvider()]
+);
+  
+const { connectors } = getDefaultWallets({
+	appName: "SuperFlow",
+	chains
+});
+  
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider
+});
+  
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider chains={chains} theme={darkTheme({
+			accentColor: 'black',
+			accentColorForeground: 'white',
+			borderRadius: 'medium',
+			fontStack: 'system',
+			overlayBlur: 'small',
+			})}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</RainbowKitProvider>
+		</WagmiConfig>
 	)
 }
 
