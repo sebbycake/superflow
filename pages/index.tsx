76580fa3d26e7f9371c8612b81cc749b38f0aa17
sebbycake <leehur_sebastian@hotmail.com>
2022-12-15 13:53:41 +0800

    Add main content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import styles from "../styles/MainContent.module.css";
 
 const Home: NextPage = () => {
 	return (
@@ -10,6 +11,46 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
+			<main className={`${styles.container} ${styles.container_flex}`}>
+				<div className={styles.information}>
+					<h1 className={styles.main_title}>
+						Crypto payments <br /> every second
+					</h1>
+					<p className={styles.main_description}>
+						SuperFlow provides an effective and powerful way to create and
+						manage your subscriptions
+					</p>
+					<button className={styles.button}>Launch App</button>
+					<div className={styles.benefits}>
+						<div className={styles.benefit}>
+							<img src="/asset1.png" alt="" className={styles.benefit_img} />
+							<p className={styles.benefit_description}>
+								Speed & <br /> Security
+							</p>
+						</div>
+						<div className={styles.benefit}>
+							<img src="/asset2.png" alt="" className={styles.benefit_img} />
+							<p className={styles.benefit_description}>
+								Flexibility & <br /> Scalability
+							</p>
+						</div>
+						<div className={styles.benefit}>
+							<img src="/asset3.png" alt="" className={styles.benefit_img} />
+							<p className={styles.benefit_description}>
+								Better <br /> Ownership
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div>
+					<img
+						src="/landing_graphic.png"
+						alt=""
+						className={styles.main_image}
+					/>
+				</div>
+			</main>
 		</div>
 	);
 };
